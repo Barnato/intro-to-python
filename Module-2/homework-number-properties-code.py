2f5894f288b1e5e72b83bb92bba91959043f3ff2
Victoria Barnato <vickybarnatosmith@gmail.com>
2024-08-20 13:37:04 -0500

    module 2 - assignment - composite numbers
    completed more of homework module 2 assignment - composite numbers

--- Module-2/homework-number-properties-code.py
+++ Module-2/homework-number-properties-code.py
@@ -18,5 +18,23 @@ factors = []
 
 print(getFactors(21))
      
+def isComposite(x):
+    if ((x > 1) and (x % 2 == 0)):
+        print("True")
+    else:
+        print("False")
+    """Returns whether or not the given number x is composite.
 
+    A composite number has more than 2 factors.
+    A natural number greater than 1 that is not prime is called a composite number.
+    Note, the number 1 is neither prime nor composite.
+
+    For example:
+    - Calling isComposite(9) will return True
+    - Calling isComposite(22) will return True
+    - Calling isComposite(3) will return False
+    - Calling isComposite(41) will return False
+    """
+    
+    # your code here
 
