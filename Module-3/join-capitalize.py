67f98e8db9c159cd84f1059a797fc68628a4ea8d
Victoria Barnato <vickybarnatosmith@gmail.com>
2024-08-25 13:42:06 -0500

    homework 3
    homework 3

---
+++ Module-3/join-capitalize.py
@@ -0,0 +1,21 @@
+
+ def capitalize_or_join_words(sentence):
+    """
+    If the given sentence starts with *, capitalizes the first and last letters of each word in the sentence,
+    and returns the sentence without *.
+    Else, joins all the words in the given sentence, separating them with a comma, and returns the result.
+ 
+    For example:
+    - If we call capitalize_or_join_words("*i love python"), we'll get "I LovE PythoN" in return.
+    - If we call capitalize_or_join_words("i love python"), we'll get "i,love,python" in return.
+    - If we call capitalize_or_join_words("i love    python  "), we'll get "i,love,python" in return.
+ 
+    Hint(s):
+    - The startswith() function checks whether a string starts with a particualr character
+    - The capitalize() function capitalizes the first letter of a string
+    - The upper() function converts all lowercase characters in a string to uppercase
+    - The join() function creates a single string from a list of multiple strings
+    """
+    # your code here
+    if sentence[0] == "*":
+        print(sentence.isupper())
