2b323531f5c2bfff0a3d2e98665048c3bacc18a2
Victoria Barnato <vickybarnatosmith@gmail.com>
2024-08-29 22:44:26 -0500

    concatenate
    concatenate

---
+++ Module-3/concatenate.py
@@ -0,0 +1,20 @@
+def concatenate(strings):
+    """
+    Concatenates the given list of strings into a single string.
+    Returns the single string.
+    If the given list is empty, returns an empty string.
+
+    For example:
+    - If we call concatenate(["a","b","c"]), we'll get "abc" in return
+    - If we call concatenate([]), we'll get "" in return
+
+    Hint(s):
+    - Remember, you can create a single string from a list of multiple strings by using the join() function
+    """
+    # your code here
+
+strings = ['a', 'b', 'c']
+concatenate_lists = ','.join(strings)
+print(concatenate_lists)
+
+
