becaf804ed259ab0b6553dccc5df51be4a1da308
Victoria Barnato <vickybarnatosmith@gmail.com>
2024-11-04 11:57:42 -0600

    Create final_assignment_module_4.py
    first attempt at bank system

---
+++ Module-4/final_assignment_module_4.py
@@ -0,0 +1,12 @@
+def import_and_create_bank(filename)
+
+bankdictionary = {}
+
+    #open the file in read mode
+    f1 = open(final_assignment_module_4, 'r+')
+
+  lines = f.readlines()
+
+  for line in lines:
+
+     lst = line.strip().spilt(",")
