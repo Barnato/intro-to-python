09e89d37508d87f3f524b5e8f160430cd05a1c51
Victoria Barnato <vickybarnatosmith@gmail.com>
2024-11-05 13:54:03 -0600

    Create log-in.py
    log-in

---
+++ Module-4/Assignment 4/log-in.py
@@ -0,0 +1,22 @@
+def login(user_accounts, log_in, username, password):
+    '''
+    This function allows users to log in with their username and password.
+    The user_accounts dictionary stores the username and associated password.
+    The log_in dictionary stores the username and associated log-in status.
+
+    If the username does not exist in user_accounts or the password is incorrect:
+    - Returns False.
+    Otherwise:
+    - Updates the user's log-in status in the log_in dictionary, setting the value to True.
+    - Returns True.
+
+    For example:
+    - Calling login(user_accounts, "Brandon", "123abcAB") will return False
+    - Calling login(user_accounts, "Brandon", "brandon123ABC") will return True
+    '''
+
+    if username in user_accounts and user_accounts[username] == password:
+        log_in[username] = True
+        return True
+    else:
+        return False
